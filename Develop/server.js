ff2d1f77ad8cc98a8a33d0853a93007bc0e22e99
K Ryan <hunt.kelsearyan@gmail.com>
2021-05-03 09:20:10 -0600

    CORS issue when deployed

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const apiRoutes = require('./Develop/Routes/APIRoute')
-const htmlRoutes = require('./Develop/Routes/HTMLRoute')
+const apiRoutes = require('./routes/apiRoutes')
+const htmlRoutes = require('./routes/htmlRoutes')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
