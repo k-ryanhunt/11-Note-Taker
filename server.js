b1da472502bcd55a781aa5eb17ea91542c6dce0b
K Ryan <hunt.kelsearyan@gmail.com>
2021-05-31 10:56:03 -0600

    Push

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = require('express');
-const apiRoutes = require('./Develop/routes/apiRoutes')
-const htmlRoutes = require('./Develop/routes/htmlRoutes')
+const express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.use(apiRoutes)
-app.use(htmlRoutes)
+app.use(apiRoutes);
+app.use(htmlRoutes);
 
-app.listen(PORT, () => console.log(`App listening on PORT: http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`App listening on PORT: http://localhost:${PORT}`)
+);
